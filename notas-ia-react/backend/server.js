6a23f4939a9ac87c17800e08690735333b5b9d30
Jorge <jorge.lazpoyeduca.jcyl.es>
2026-01-19 21:34:39 +0100

    Utilizacion de ia gratuita, groq, cambio en el servidor y en .env donde esta la api

--- notas-ia-react/backend/server.js
+++ notas-ia-react/backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
-import OpenAI from "openai";
 import dotenv from "dotenv";
+import Groq from "groq-sdk";
 
 dotenv.config();
 
@@ -9,8 +9,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const client = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+const groq = new Groq({
+  apiKey: process.env.GROQ_API_KEY,
 });
 
 app.post("/resumir", async (req, res) => {
@@ -21,20 +21,30 @@ app.post("/resumir", async (req, res) => {
   }
 
   try {
-    const completion = await client.chat.completions.create({
-      model: "gpt-4.1-mini",
+    const completion = await groq.chat.completions.create({
+      model: "llama-3.1-8b-instant",
+
       messages: [
-        { role: "system", content: "Eres un asistente que resume textos." },
-        { role: "user", content: `Resume el siguiente texto:\n\n${texto}` }
+        {
+          role: "system",
+          content: "Eres un asistente que resume textos en español."
+        },
+        {
+          role: "user",
+          content: `Resume este texto:\n\n${texto}`
+        }
       ],
+      temperature: 0.3,
+      max_tokens: 200,
     });
 
     const resumen = completion.choices[0].message.content;
 
     res.json({ resumen });
+
   } catch (error) {
-    console.error("Error OpenAI:", error);
-    res.status(500).json({ error: "Error al generar el resumen" });
+    console.error("Error Groq:", error);
+    res.status(500).json({ error: "Error al generar resumen con IA" });
   }
 });
 
